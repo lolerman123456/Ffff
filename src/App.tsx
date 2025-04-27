28e5c3d33887c6df6fcca6638f821531c35e2711
lolerman123456 <78628284+lolerman123456@users.noreply.github.com>
2025-04-26 21:44:02 -0400

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100vh", fontSize: "24px" }}>
+      Zoned App Ready
+    </div>
+  );
+}
